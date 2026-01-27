184bc4a9219a3ca444951b91b8958a1c5b3a4c83
Candela Jazmín Gutiérrez González <candeguti84@gmail.com>
2026-01-27 19:32:24 +0100

    feat: Actualización del repositorio Home incorporando la lógica de schemas

--- backend/app/repositories/home_repository.py
+++ backend/app/repositories/home_repository.py
@@ -1,24 +1,132 @@
-from Indoora.backend.app.models.home import Home
+from Indoora.backend.app.models.home import Home, Position, Room
+from Indoora.backend.app.schemas.home import HomeCreate, HomeUpdate, PositionCreate, RoomCreate
 from sqlmodel import Session, select
 
 class HomeRepository:
     def __init__(self, session: Session):
         self.session = session
     
-    def create(self, home: Home) -> Home:
+    # ------------Home------------
+
+    def create_home(self, data: HomeCreate) -> Home:
+        home = Home(
+            name = data.name,
+            subject_id = data.subject_id
+        )
+
         self.session.add(home)
         self.session.commit()
         self.session.refresh(home)
         return home
     
-    def update(self, home: Home) -> Home:
-        self.session.add(home)
-        self.session.commit()
-        self.session.refresh(home)
-        return home
-    
-    def get_all(self) -> list[Home]:
-        return self.session.exec(select(Home)).all()
-    
-    def get_by_id(self, home_id: int) -> Home | None:
+    def get_home_by_id(self, home_id: int) -> Home | None:
         return self.session.get(Home, home_id)
+
+    def get_all_homes(self) -> list[Home]:
+        return self.session.exec(select(Home)).all()   
+    
+    def update(self, home_id: int, data: HomeUpdate) -> Home:
+        home = self.get_home_by_id(home_id)
+        if not home:
+            raise ValueError("Home not found")
+        
+        update_data = data.dict(exclude_unset=True)
+
+        for key, value in update_data.items():
+            setattr(home, key, value)
+
+        self.session.commit()
+        self.session.refresh(home)
+        return home
+    
+    def delete_home(self, home_id: int):
+        home = self.get_home_by_id(home_id) 
+        if not home:
+            raise ValueError("Home not found")
+        
+        self.session.delete(home)
+        self.session.commit()
+    
+
+    # ------------Room------------
+    
+    def create_room(self, data: RoomCreate) -> Room:
+        room = Room(
+            name = data.name,
+            home_id = data.home_id
+        )
+
+        self.session.add(room)
+        self.session.commit()
+        self.session.refresh(room)
+        return room
+    
+    def get_room_by_id(self, room_id: int) -> Room | None:
+        return self.session.get(Room, room_id)
+
+    def get_all_rooms(self) -> list[Room]:
+        return self.session.exec(select(Room)).all()
+    
+    def update_room(self, room_id: int, data: RoomCreate) -> Room:
+        room = self.get_room_by_id(room_id)
+        if not room:
+            raise ValueError("Room not found")
+        
+        update_data = data.dict(exclude_unset=True)
+
+        for key, value in update_data.items():
+            setattr(room, key, value)
+
+        self.session.commit()
+        self.session.refresh(room)
+        return room
+    
+    def delete_room(self, room_id: int):
+        room = self.get_room_by_id(room_id)
+        if not room:
+            raise ValueError("Room not found")
+        
+        self.session.delete(room)
+        self.session.commit()   
+
+    
+    # ------------Position------------
+
+    def create_position(self, data: PositionCreate) -> Position:
+        position = Position(
+            name = data.name,
+            room_id = data.room_id
+        )
+
+        self.session.add(position)
+        self.session.commit()
+        self.session.refresh(position)
+        return position
+    
+    def get_position_by_id(self, position_id: int) -> Position | None:
+        return self.session.get(Position, position_id)
+
+    def get_all_positions(self) -> list[Position]:
+        return self.session.exec(select(Position)).all()
+    
+    def update_position(self, position_id: int, data: PositionCreate) -> PositionCreate:
+        position = self.get_room_by_id(position_id)
+        if not position:
+            raise ValueError("Position not found")
+        
+        update_data = data.dict(exclude_unset=True)
+
+        for key, value in update_data.items():
+            setattr(position, key, value)
+
+        self.session.commit()
+        self.session.refresh(position)
+        return position
+    
+    def delete_position(self, position_id: int):
+        position = self.get_room_by_id(position_id)
+        if not position:
+            raise ValueError("Position not found")
+        
+        self.session.delete(position)
+        self.session.commit()   
