cecad196486b10c701e3fce0b03eeb194f0cf8e3
Candela Jazmín Gutiérrez González <candeguti84@gmail.com>
2026-02-02 13:53:14 +0100

    feat: Actualización de dependencias e inicialización de módulos python

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,4 @@
+from .user import User
+from .home import Home, Room, Activity, Position, ActivityPosition
+from .device import EmisorDevice, ReceptorDevice
+from .routine import Routine, DayRoutine, DayRoutineLink
